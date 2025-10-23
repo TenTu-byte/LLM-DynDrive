3963e03c24a66b257edc86e55be4f3486035004f
l50055018 <liyulin39@huawei.com>
2025-10-23 14:37:38 +0800

    Delete all files

--- scripts_yulin/gitcode_downloader.py
+++
@@ -1,4 +0,0 @@
-from gitcode_hub import snapshot_download
-
-
-snapshot_download("ascend-tribe/openPangu-Embedded-7B-V1.1",  local_dir = 'D:\\models\\openPangu-Embedded-7B-V1.1')
--- scripts_yulin/merge_shards.sh
+++
@@ -1,6 +0,0 @@
-PROJECT_DIR=$(dirname "$(dirname "$(realpath "$0")")")
-cd ${PROJECT_DIR}
-
-python merge_shards.py \
-  --dir "$DATASET_DIR/outputs_yulin/openPangu-Embedded-7B-V1.1/Math_Math"\
-  --base 'origin_temp0.7_maxlen16000'
