c925884e7942f8680ed45a1fa6446655df67af0d
l50055018 <liyulin39@huawei.com>
2025-10-26 07:22:37 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 SCRIPTS_DIR="./scripts_yulin"
-SCRIPT_NAME="extract_hidden_pangu_dist.sh"
+SCRIPT_NAME="dynamic_steer_pangu_dist.sh"
 SCRIPT_PATH="$SCRIPTS_DIR/$SCRIPT_NAME"
 
 dos2unix "$SCRIPT_PATH"
