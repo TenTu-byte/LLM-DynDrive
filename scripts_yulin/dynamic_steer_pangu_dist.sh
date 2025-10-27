af567ef0172b0a2c59dd3b993e1d5baff63053fc
l50055018 <liyulin39@huawei.com>
2025-10-27 19:41:49 +0800

    Ajust the dynamic steering script's parameters

--- scripts_yulin/dynamic_steer_pangu_dist.sh
+++ scripts_yulin/dynamic_steer_pangu_dist.sh
@@ -64,12 +64,6 @@ for ds in "${datasets[@]}"; do
         --run_id $run_id \
         --max_generated_tokens $max_tokens \
         --seed $seed \
-        --q25c $q25c \
-        --q75c $q75c \
-        --low_val_1 $low_val_1 \
-        --high_val_1 $high_val_1 \
-        --q25v $q25v \
-        --q75v $q75v \
         --low_val_2 $low_val_2 \
         --high_val_2 $high_val_2
     echo "=== Finished ${ds} ==="
@@ -89,3 +83,10 @@ done
 # all:
 # max_tokensS=16000
 # seed=42
+
+# --q25c $q25c \
+# --q75c $q75c \
+# --low_val_1 $low_val_1 \
+# --high_val_1 $high_val_1 \
+# --q25v $q25v \
+# --q75v $q75v \
