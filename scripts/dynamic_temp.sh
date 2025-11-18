b891fd63ff95803d6722e1e46f9fdd42f6feb42d
l50055018 <liyulin39@huawei.com>
2025-11-18 17:06:29 +0800

    Fix script bug

--- scripts/dynamic_temp.sh
+++ scripts/dynamic_temp.sh
@@ -36,13 +36,13 @@ for ds in "${datasets[@]}"; do
     python -u transformer_inference_temp_dp.py \
         --model_name_or_path "$models/$model_name" \
         --dataset_dir "./Data/" \
-        --output_path "$outputs/outputs_temp_dynamic/$model_name" \
+        --output_path "$outputs/outputs_temp_dynamic/" \
         --dataset "$ds" \
         --max_generated_tokens $max_tokens \
         --num_gpus 8 \
         --steer_vector_path "$outputs/vectors/$model_name/steer_vector.pt" \
         --steer_layer 58 \
         --steer_coef -1 \
-        --temperatuyre 1
+        --temperature 1
     echo "=== Finished ${ds} ==="
 done
