fad4431dffff5cc29cce994ca1d3d3cd38fe0b1d
Yulin Li <1617670866@qq.com>
2025-11-18 16:06:59 +0800

    transfer setup

--- scripts/config_env.sh
+++ scripts/config_env.sh
@@ -1,3 +1,8 @@
 # CUDA 12.1
 pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1
 pip install transformers==4.57.1
+
+# Install wheels
+pip freeze > requirements_transfer.txt
+mkdir wheels
+pip wheel -r requirements_transfer.txt -w wheels/
