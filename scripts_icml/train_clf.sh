437d7fa3f9a5655087f2a17bbd21a97edcdc2dc3
l50055018 <liyulin39@huawei.com>
2025-12-24 23:03:31 +0800

    Update training scripts

--- scripts_icml/train_clf.sh
+++ scripts_icml/train_clf.sh
@@ -6,9 +6,9 @@ PROJECT_DIR=$(dirname "$(dirname "$(realpath "$0")")")
 cd ${PROJECT_DIR}
 
 python train_clf.py \
-  --folder "$outputs/icml/openPangu-Embedded-7B-V1.1/Math_Math" \
+  --folder "$DATASET_DIR/outputs_yulin_gy/icml/openPangu-Embedded-7B-V1.1/Math_Math" \
   --k -1 \
   --layer -1 \
   --max_points 50000 \
-  --out_dir "$outputs/icml/openPangu-Embedded-7B-V1.1/Math_Math/classifer/remain_clf_allk" \
+  --out_dir "$DATASET_DIR/outputs_yulin_gy/icml/openPangu-Embedded-7B-V1.1/Math_Math/classifer/remain_clf_allk" \
   --pos_is_short
